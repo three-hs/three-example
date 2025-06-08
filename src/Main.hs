23e3824e361bebad5cc6ed36723ee601540d1441
Julien Dehos <dehos@univ-littoral.fr>
2025-06-08 18:11:35 +0200

    test AmbientLight

--- src/Main.hs
+++ src/Main.hs
@@ -5,9 +5,10 @@ module Main where
 
 import Language.Javascript.JSaddle as JS
 import Miso 
-import Miso.Media
-import Miso.String 
+import Miso.String (ms)
 
+import THREE.AmbientLight
+import THREE.Light
 import THREE.Mesh
 
 -------------------------------------------------------------------------------
@@ -21,16 +22,16 @@ app = do
   Image image1 <- newImage "favicon.ico"
   valToStr image1 >>= consoleLog
 
-  -----------------------------------------------------------------------------
-  -- doesn't work since Miso.Media.new try to create a 'Media' object (but JS
-  -- only knows Audio or Video).
-
-  -- Media media1 <- Miso.Media.new "favicon.ico"
-  -- valToStr media1 >>= consoleLog
-  -----------------------------------------------------------------------------
-
   mesh1 <- THREE.Mesh.new
   valToStr (unMesh mesh1) >>= consoleLog
+
+  light1 <- THREE.AmbientLight.new 0 0.5
+  valToStr (unAmbientLight light1) >>= consoleLog
+  intensity light1 >>= consoleLog . ms
+  dispose light1
+
+  light2 <- THREE.AmbientLight.new 0 0.42
+  copy light1 light2 >>= intensity >>= consoleLog . ms
 
   consoleLog "end"
 
