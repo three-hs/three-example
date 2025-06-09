138f03c40f380ad2f8645c0fe6322a60140f678f
Julien Dehos <dehos@univ-littoral.fr>
2025-06-09 18:27:40 +0200

    bump (lights branch)

--- src/Main.hs
+++ src/Main.hs
@@ -9,35 +9,27 @@ import Miso.String (ms)
 
 import THREE.AmbientLight
 import THREE.Light
-import THREE.Mesh
 
--------------------------------------------------------------------------------
--- test app
--------------------------------------------------------------------------------
+
+testLights :: JSM ()
+testLights = do
+  ambientLight1 <- THREE.AmbientLight.new 0 0.01
+  valToStr (unAmbientLight ambientLight1) >>= consoleLog
+  intensity ambientLight1 >>= consoleLog . ms
+  dispose ambientLight1
+  ambientLight2 <- THREE.AmbientLight.new 0 0.42
+  copy ambientLight1 ambientLight2
+  intensity ambientLight1 >>= consoleLog . ms
+
+  pointLight1 <- THREE.AmbientLight.new 0 0.03
+  valToStr (unAmbientLight pointLight1) >>= consoleLog
+  intensity pointLight1 >>= consoleLog . ms
 
 app :: JSM ()
 app = do
   consoleLog "begin"
-
-  Image image1 <- newImage "favicon.ico"
-  valToStr image1 >>= consoleLog
-
-  mesh1 <- THREE.Mesh.new
-  valToStr (unMesh mesh1) >>= consoleLog
-
-  light1 <- THREE.AmbientLight.new 0 0.5
-  valToStr (unAmbientLight light1) >>= consoleLog
-  intensity light1 >>= consoleLog . ms
-  dispose light1
-
-  light2 <- THREE.AmbientLight.new 0 0.42
-  copy light1 light2 >>= intensity >>= consoleLog . ms
-
+  testLights
   consoleLog "end"
-
--------------------------------------------------------------------------------
--- main
--------------------------------------------------------------------------------
 
 #ifdef WASM
 foreign export javascript "hs_start" main :: IO ()
